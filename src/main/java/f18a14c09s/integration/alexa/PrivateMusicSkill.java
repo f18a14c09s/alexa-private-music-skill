6cd708283e16f30eb4b106e3c641ae020f8d6f4c
Francis Johnson <business@francisjohnson.org>
2022-09-02 00:54:27 -0400

    Reverted to simpler solution that suffices and is more helpful in some ways.

--- src/main/java/f18a14c09s/integration/alexa/PrivateMusicSkill.java
+++ src/main/java/f18a14c09s/integration/alexa/PrivateMusicSkill.java
@@ -8,6 +8,7 @@ import com.amazon.ask.util.impl.JacksonJsonMarshaller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import f18a14c09s.integration.alexa.data.AbstractMessage;
 import f18a14c09s.integration.alexa.data.BaseMessage;
+import f18a14c09s.integration.alexa.data.MessageHeader;
 import f18a14c09s.integration.alexa.music.data.RequestType;
 import f18a14c09s.integration.alexa.music.messagetypes.*;
 import f18a14c09s.integration.json.JSONAdapter;
@@ -16,10 +17,15 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.util.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
-public class PrivateMusicSkill implements AlexaSkill<Request, Response> {
-    private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+public class PrivateMusicSkill implements AlexaSkill<Request<?>, Response<?>> {
+    private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
     private JSONAdapter jsonAdapter = new JSONAdapter();
     private static final Map<RequestType, AbstractMusicSkill<? extends Request<?>, ? extends Response<?>>>
             skillsByRequestType;
@@ -40,34 +46,68 @@ public class PrivateMusicSkill implements AlexaSkill<Request, Response> {
     }
 
     @Override
-    public SkillResponse<Response> execute(SkillRequest skillRequest, Object context) {
-        Optional<Request> request;
+    public SkillResponse<Response<?>> execute(SkillRequest skillRequest, Object context) {
+        // In theory, the Alexa Skills Kit SDK will
+        Request<?> request;
+        String rawSkillRequestString;
         try {
-            request = Optional.ofNullable(jsonAdapter.readValue(skillRequest.getRawRequest(), Request.class));
+            rawSkillRequestString = StandardCharsets.UTF_8.newDecoder()
+                    .onMalformedInput(CodingErrorAction.REPORT)
+                    .onUnmappableCharacter(CodingErrorAction.REPORT)
+                    .decode(
+                            ByteBuffer.wrap(skillRequest.getRawRequest())
+                    )
+                    .toString();
         } catch (IOException e) {
-            logger.error(e);
+            LOGGER.error("Failed to parse raw skill request as a UTF-8 string.", e);
             return null;
         }
-        request.ifPresent(this::debug);
-        SkillResponse<Response> retval = request.map(BaseMessage::getHeader)
+        LOGGER.debug(String.format(
+                "Raw skill request string:%n%s",
+                rawSkillRequestString
+        ));
+        try {
+            request = jsonAdapter.readValue(rawSkillRequestString, Request.class);
+        } catch (IOException e) {
+            LOGGER.error("Failed to parse skill request string as JSON.", e);
+            return null;
+        }
+        if (request == null) {
+            LOGGER.error("Raw skill request JSON parsed as null value.");
+            return null;
+        }
+        debug(request);
+        AbstractMusicSkill<?, ?> skillOperation = Optional.of(request).map(BaseMessage::getHeader)
                 .map(messageHeader -> new RequestType(messageHeader.getNamespace(), messageHeader.getName()))
-                .map(skillsByRequestType::get)
-                .map(skill -> skill.invoke(request.get(), context))
-                .map(response -> new BaseSkillResponse(new JacksonJsonMarshaller(), response))
-                .orElse(null);
-        this.debug(retval.getResponse());
-        return retval;
+                .map(skillsByRequestType::get).orElse(null);
+        if (skillOperation == null) {
+            LOGGER.error(String.format(
+                    "Skill operation identified by namespace (%s) and/or name (%s) not found.",
+                    Optional.ofNullable(request.getHeader())
+                            .map(MessageHeader::getNamespace)
+                            .orElse("N/A; header is null"),
+                    Optional.ofNullable(request.getHeader()).map(MessageHeader::getName).orElse("N/A; header is null")
+            ));
+            return null;
+        }
+        Response<?> operationResponse = skillOperation.invoke(request, context);
+        if (operationResponse == null) {
+            LOGGER.warn("Skill operation returned null.");
+            return null;
+        }
+        debug(operationResponse);
+        return new BaseSkillResponse<>(new JacksonJsonMarshaller<>(), operationResponse);
     }
 
     private void debug(AbstractMessage<?> message) {
         String messageId = message.getHeader().getMessageId();
         try {
-            logger.debug(String.format("%s message %s:%n%s",
+            LOGGER.debug(String.format("%s message %s:%n%s",
                     message.getHeader().getName(),
                     messageId,
                     jsonAdapter.writeValueAsString(message)));
         } catch (JsonProcessingException e) {
-            logger.warn(String.format("Failed to print message %s.", messageId), e);
+            LOGGER.warn(String.format("Failed to print message %s.", messageId), e);
         }
     }
 }
