8b8a5e157987975b9726b59ae6d9111f7662d1ed
f18a14c09s <35183457+f18a14c09s@users.noreply.github.com>
2022-09-02 09:28:40 -0400

    Moved in existing int test directory.

---
+++ integration-tests/src/test/java/f18a14c09s/integration/alexa/music/privatemusicskill/getplayablecontent/MainFlowIntTest.java
@@ -0,0 +1,79 @@
+package f18a14c09s.integration.alexa.music.privatemusicskill.getplayablecontent;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvokeRequest;
+import software.amazon.awssdk.services.lambda.model.InvokeResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainFlowIntTest {
+    static LambdaClient lambdaClient;
+    static ObjectMapper jsonMapper;
+
+    @BeforeAll
+    static void beforeAll() {
+        lambdaClient = LambdaClient.builder().build();
+        jsonMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+
+    SdkBytes requestPayload(String testCaseFilenamePrefix) {
+        return assertDoesNotThrow(() -> {
+            try (InputStream testCaseInputStream = getClass().getClassLoader().getResourceAsStream(
+                    String.format(
+                            "privatemusicskill/getplayablecontent/%s_request.json",
+                            testCaseFilenamePrefix
+                    )
+            )) {
+                return SdkBytes.fromInputStream(testCaseInputStream);
+            }
+        });
+    }
+
+    Map<?, ?> expectedDecodedPayload(String testCaseFilenamePrefix) {
+        return assertDoesNotThrow(() -> {
+            try (InputStream expectedResponseInputStream = getClass().getClassLoader().getResourceAsStream(
+                    String.format(
+                            "privatemusicskill/getplayablecontent/%s_response.json",
+                            testCaseFilenamePrefix
+                    )
+            )) {
+                return jsonMapper.readValue(expectedResponseInputStream, Map.class);
+            }
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+            strings = {
+                    "artist", "album", "track"
+            }
+    )
+    void testByEntityType(
+            String testCaseFilenamePrefix
+    ) throws IOException {
+        InvokeResponse actualResponse = lambdaClient.invoke(InvokeRequest.builder().functionName(
+                "privateMusicAlexaSkill"
+        ).payload(
+                requestPayload(testCaseFilenamePrefix)
+        ).build());
+
+        assertNotNull(actualResponse);
+        String actualPayloadJson = actualResponse.payload().asUtf8String();
+        assertNotNull(actualPayloadJson);
+        assertEquals(
+                jsonMapper.readValue(actualResponse.payload().asUtf8String(), Map.class),
+                expectedDecodedPayload(testCaseFilenamePrefix),
+                actualResponse.payload().asUtf8String()
+        );
+    }
+}
